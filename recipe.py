24bbd47ed5a32c66d9ceea073fccbf267f7d293f
Simeon Ehrig <s.ehrig@hzdr.de>
2019-09-09 08:29:29 +0200

    Add hppcm container recipe for the example container
    - hppcm: https://github.com/NVIDIA/hpc-container-maker
    - only singularity is supported
    - at the moment only single-stage build is supported
    - change jupyter notebook and add .ipynb_checkpoints -> this should cause, that the example is open by default

---
+++ recipe.py
@@ -0,0 +1,67 @@
+import argparse, sys
+
+# release container from xeus-cling-cuda-container project
+# https://github.com/ComputationalRadiationPhysics/xeus-cling-cuda-container
+import xeusClingCudaContainer.hpccm.rel_container as relc
+
+from hpccm.primitives import copy, shell, runscript
+from hpccm.templates.CMakeBuild import CMakeBuild
+from hpccm.building_blocks.packages import packages
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Simple script for generating a singularity recipe for the GOL example.')
+    parser.add_argument('--build_dir', type=str, default='/tmp/GOL_example',
+                        help='Define the path in which all projects will be built (default: /tmp/GOL_example).')
+    parser.add_argument('-j', type=str, help='number of build threads for make (default: -j)')
+    args = parser.parse_args()
+
+    if args.j:
+        threads = int(args.j)
+        if threads < 1:
+            raise ValueError('-j have to be greater than 0')
+    else:
+        threads=None
+
+    stages = relc.gen_stage('singularity', args.build_dir, 'RELEASE', True, threads)
+    if type(stages) is not list:
+        singleStageBuild(stages)
+    else:
+        multiStageBuild(stages)
+
+def singleStageBuild(stage):
+    # copy example inside container
+    stage += copy(src='notebook', dest='/')
+
+    # copy and build the pnwriter library
+    stage += copy(src='pngwriter', dest='/opt')
+    stage += packages(ospackages=['libpng-dev'])
+    cmake = CMakeBuild(prefix='/notebook/pngwriter')
+    stage += shell(commands=[cmake.configure_step(directory='/opt/pngwriter',
+                                                  opts=['-DBUILD_SHARED_LIBS=ON']),
+                             cmake.build_step(target='install'),
+                             'rm -rf /opt/pngwriter'])
+
+    # Copy notebook examples and pngwriter lib to the host's /tmp file system to obtain a writable file system.
+    stage += runscript(commands=['if [ ! -d /tmp/GOL-xeus-cling-cuda ]; then \n'
+                                 ' mkdir /tmp/GOL-xeus-cling-cuda &&'
+                                 ' cp -r /notebook/ /tmp/GOL-xeus-cling-cuda\n fi',
+                                 'cd /tmp/GOL-xeus-cling-cuda/notebook',
+                                 'jupyter-lab'])
+
+    print(stage.__str__())
+
+def multiStageBuild(stages):
+    print('multi-stage-builds are not supported at the moment', file=sys.stderr)
+
+    sys.exit(1)
+
+    # TODO: for a later implementation
+    recipe = []
+    for stage in stages:
+            recipe.append('')
+            recipe.append(stage.__str__())
+    print('\n'.join(recipe))
+
+if __name__ == '__main__':
+    main()
