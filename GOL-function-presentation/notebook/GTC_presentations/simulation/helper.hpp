610ed42de1559f46f4ef008b1fa5e2b13efdd8bc
Simeon Ehrig <s.ehrig@hzdr.de>
2020-03-20 18:23:07 +0100

    Add some newlines at the end of files

--- GOL-function-presentation/notebook/GTC_presentations/simulation/helper.hpp
+++ GOL-function-presentation/notebook/GTC_presentations/simulation/helper.hpp
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 // checks whether the return value was successful
 // If not, print an error message
@@ -13,7 +13,7 @@ void display_image(std::vector< unsigned char> & image, bool clear_ouput){
     // memory objects for output in the web browser
     std::stringstream buffer;
     xeus::xjson mine;
-    
+
     if(clear_ouput)
         xeus::get_interpreter().clear_output(true);
 
@@ -21,7 +21,7 @@ void display_image(std::vector< unsigned char> & image, bool clear_ouput){
     for(auto c : image){
         buffer << c;
     }
-    
+
     mine["image/png"] = xtl::base64encode(buffer.str());
     xeus::get_interpreter().display_data(
         std::move(mine),
